be2ac0f1aa0b48abbe69566497ad89970a39911b
Taha <t2905j@gmail.com>
2021-01-12 16:42:25 +0530

    Created first iinstallation

---
+++ Assets/BoardBlocks.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BoardBlocks : MonoBehaviour
+{
+
+    public Button button;
+    public Text buttonText;
+
+    private GC gameController;
+
+    public void SetGameControllerReference(GC controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+}
---
+++ Assets/GC.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GC : MonoBehaviour
+{
+
+    public Text[] buttonList;
+
+    private string playerSide;
+
+    public GameObject m_gameOverPanel;
+    public Text m_gameOverText;
+
+    private int m_count;
+    private int flag;
+
+    public GameObject m_restartButton;
+
+    void Awake()
+    {
+        m_gameOverPanel.SetActive(false);
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X";
+        m_count = 0;flag = 0;
+        m_restartButton.SetActive(false);
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<BoardBlocks>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+
+        ChangeSides();
+        m_count++;
+        if(m_count >= 9)
+        {
+            SetGameOverText("Draw");
+        }
+
+    }
+
+    void ChangeSides()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+        SetGameOverText(playerSide + " Wins!!");
+        
+    }
+
+    void SetGameOverText(string s)
+    {
+        m_restartButton.SetActive(true);
+        m_gameOverPanel.SetActive(true);
+        m_gameOverText.text = s;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
